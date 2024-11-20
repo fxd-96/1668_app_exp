fc81ab4b40a48ab34cc8f711926ff6f0a27a42eb
Flavio Dorado Montoya <123769391+fxd-96@users.noreply.github.com>
2024-11-19 22:29:10 -0600

    regresar a original

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.post('/insert', jsonParser, function (req, res) {
           return;
 
         } else {
-          console.log("Insercion fue correcta");
+          console.log("Insert was successful!");
         }
     });
 
